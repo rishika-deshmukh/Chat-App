b94e4ed3f70e0f6a0bfa590d01302b88d603f3b5
Samriddhi <samriddhiiisingh@gmail.com>
2025-02-13 02:05:42 +0530

    MONGODB WORKS!

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,4 @@
+import { compare } from "bcrypt";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 const { sign } = jwt;
@@ -24,6 +25,43 @@ export const signup = async(request, response, next)=>{
             email:user.email,
        
             profileSetup: user.profileSetup,
+
+        }})
+    }catch(error){
+        console.log({ error });
+        return response.status(500).send("Internal server error.");
+    }
+};
+export const login = async(request, response, next)=>{
+    try{
+        const {email, password}= request.body;
+        if(!email || !password){
+            return response.status(400).send("Email and password is required.");
+
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return response.status(404).send("User with the given email not found.");
+        }
+        const auth = await compare(password, user.password);
+        if(!auth){
+            return response.status(400).send("Password is incorrect");
+        }
+
+        response.cookie("jwt",createToken(email,user.id),{
+            maxAge,
+            secure:true,
+            sameSite: "None",
+        })
+        return response.status(200).json({user:{
+            id:user.id,
+            email:user.email,
+            profileSetup: user.profileSetup,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            image:user.image,
+            color:user.color,
+
 
         }})
     }catch(error){
