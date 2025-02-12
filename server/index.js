b64b288465fd31589aa971db4ababf936afa9dab
naritas2006 <naritas0206@gmail.com>
2025-02-12 22:29:57 +0530

    my changes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,8 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/api/auth",authRoutes);
 
 // Server listening
 const server = app.listen(port, () => {
