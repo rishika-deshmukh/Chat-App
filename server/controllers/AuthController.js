52b74c4dbde0a6bbf7c1276a69b1a028af12d3dc
Samriddhi <samriddhiiisingh@gmail.com>
2025-02-12 22:28:11 +0530

    authcontroller

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -2,7 +2,7 @@ import User from "../models/UserModel";
 import {sign} from "jsonwebtoken"
 const maxAge = 3 * 24 * 60 * 1000;
 const createToken = {email,userId}=>{
-    return sign({email, userId})
+    return sign({email, userId},process.env.JWT_KEY,{expiresIn: maxAge})
 }
 export const signup = async(request, response, next)=>{
     try{
@@ -12,10 +12,20 @@ export const signup = async(request, response, next)=>{
 
         }
         const user = await User.create({email, password});
+        response.cookie("jwt",createToken(email,user.id),{
+            maxAge,
+            secure:true;
+            sameSite: "None",
+        })
+        return response.status(201).json({user:{
+            id:user.id,
+            email:user.email,
+       
+            profileSetup: user.profileSetup,
 
+        }})
     }catch(error){
         console.log({ error });
         return response.status(500).send("Internal server error.");
     }
 };
-//hi sam
