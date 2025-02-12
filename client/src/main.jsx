857dc9b5d7b03355d72f0d215ece2692bcfb3b40
Samriddhi <samriddhiiisingh@gmail.com>
2025-02-12 23:15:18 +0530

    done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from './components/ui/sonner'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <App />
-  </StrictMode>,
-)
+    <Toaster closeButton />
+
+  </>
+);
