33b83c5cbd914e62c0036ab72ebc461b3a62606e
rishika-deshmukh <163396118+rishika-deshmukh@users.noreply.github.com>
2025-02-22 22:52:36 +0530

    Update index.jsx

--- client/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -9,6 +9,11 @@ const EmptyChatContainer= () => {
             width={200}
             options={animationDefaultOptions}
             />
+            <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center">
+                <h3 className="poppins-medium">
+                    Hi<span className="text-purple-500">!</span>Welcome to<span className="text-purple-500" Syncronus </h3>span>Chat App <span className="text-purple-500">.</span>
+                </h3>
+            </div> 
         </div>
     );
 };
